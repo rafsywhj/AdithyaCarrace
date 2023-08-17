b57fc6d3c89c56bbb765f925a28c7d69217c885c
rafsywhj <70297474+rafsywhj@users.noreply.github.com>
2023-08-17 05:01:01 +0400

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -84,7 +84,7 @@ class Game {
           this.handleFuel(index)
           this.handlePowerCoins(index)
           camera.position.x = cars[index-1].position.x
-          camera.position.y = cars[index-1].positionY
+          camera.position.y = cars[index-1].position.y
         }
       }
 
